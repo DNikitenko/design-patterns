d9f511da30f7e329981480f3973027aefb96db5c
DNikitenk <dmitry@vladivostok.com>
2015-02-23 17:33:46 +1000

    New Strategy pattern added

---
+++ Patterns/Strategy/Interfaces/IValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Strategy.Interfaces
+{
+    /// <summary>
+    /// Provides basic interface for string validators
+    /// </summary>
+    interface IValidator
+    {
+        /// <summary>
+        /// Check if string is correct
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Validate(string value);
+    }
+}
